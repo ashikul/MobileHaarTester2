f6d58789a53bb43082d187eef526cee086992113
Ashikul Alam <alam.ashikul@gmail.com>
2015-04-27 17:12:09 -0400

    import files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fruit Detection App
+
+android app with openCV using haar feature-based cascade to detect a banana
+
+
+Credits:
+http://coding-robin.de/2013/07/22/train-your-own-opencv-haar-classifier.html
+https://github.com/ferydjzz/FaceDetectionAndroidOpenCV
+https://github.com/sherrardTr4129/MobileHaarTesterAndroid
+https://github.com/sherrardTr4129/Haar_Cascade_Classifiers
---
+++ openCVSamplefacedetection/src/main/jni/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#OPENCV_CAMERA_MODULES:=off
+#OPENCV_INSTALL_MODULES:=off
+#OPENCV_LIB_TYPE:=SHARED
+include ../../sdk/native/jni/OpenCV.mk
+
+#opencv
+OPENCVROOT:= C\:\\Users\\Ashikul\\Desktop\\ndk-opencv-android-sample\\OpenCV-2.4.10-android-sdk
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
+
+
+
+LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_LDLIBS     += -llog -ldl
+
+LOCAL_MODULE     := MyLib
+
+include $(BUILD_SHARED_LIBRARY)
